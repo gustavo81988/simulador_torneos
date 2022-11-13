804dbece7f8688e3eccb34981a0ea040cbac1123
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-13 17:39:06 -0400

    avance

--- app/Services/Torneo.php
+++ app/Services/Torneo.php
@@ -9,6 +9,7 @@ class Torneo
     public function __construct($jugadores){
         $this->jugadores = $jugadores;
         $encuentros = $this->generarEncuentros($jugadores);
+        dd($encuentros);
     }
 
     public function generarEncuentros($jugadores){
@@ -21,7 +22,12 @@ class Torneo
             $ganadores[] = $this->competir($jugador1,$jugador2);
         }
 
-        return $ganadores;
+        if(count($ganadores) != 2){
+            return $this->generarEncuentros($ganadores);
+        }else{
+            dd($ganadores);
+            return $ganadores;
+        }
     }
 
     public function competir($jugador1,$jugador2){
