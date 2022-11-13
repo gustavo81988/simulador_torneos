fcfd1fd396a2523a8e34ce16b51441d8afcc7a01
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-13 17:02:47 -0400

    avance

--- app/Services/Torneo.php
+++ app/Services/Torneo.php
@@ -15,14 +15,11 @@ class Torneo
         $jugadores = $this->jugadores;
         shuffle($jugadores);
         $encuentros = array_chunk($jugadores,2);
-        return $encuentros;
-    }
-
-    public function rondaEliminatoria($encuentros){
         foreach($encuentros as $encuentro){
             $jugador1 = $encuentro[0];
             $jugador2 = $encuentro[1];
             $this->competir($jugador1,$jugador2);
         }
+        return $encuentros;
     }
 }
