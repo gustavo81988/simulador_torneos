5a583461ce6da9d635e011ff9a59703519450a15
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-13 12:46:03 -0400

    avance

---
+++ app/Services/Partido.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Services;
+
+class Partido
+{
+    public $jugador1;
+    public $jugador2;
+
+    public function __construct($jugador1,$jugador2){
+        $this->$jugador1 = $jugador1;
+        $this->$jugador2 = $jugador2;
+    }
+
+
+}
