b3c4275e232999ace860725560dc6340ebfb29de
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-13 12:24:43 -0400

    avance

--- app/Http/Controllers/TenisController.php
+++ app/Http/Controllers/TenisController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Services\Jugador;
 use App\Services\JugadorMasculino;
+use App\Services\Torneo;
 
 class TenisController extends Controller
 {
