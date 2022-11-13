350948664f7bd3e5213271a1f5a0cbdcc757eaf7
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-13 12:37:22 -0400

    avance

--- app/Services/Torneo.php
+++ app/Services/Torneo.php
@@ -3,17 +3,10 @@ namespace App\Services;
 
 class Torneo
 {
-    public function __construct(
-        $primerNombre, $segundoNombre, $habilidad,
-        $fuerza,$velocidad
-    ){
-        $this->primerNombre  = $primerNombre;
-        $this->segundoNombre = $segundoNombre;
-        $this->habilidad     = $habilidad;
-        $this->fuerza        = $fuerza;
-        $this->velocidad     = $velocidad;
+    public function __construct(){
+        $jugadores
     }
-    
+
     public function generarEncuentros($jugadores){
         shuffle($jugadores);
         $encuentros = array_chunk($jugadores,2);
