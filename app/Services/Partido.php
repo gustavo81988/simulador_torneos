856973254639ec36896a8b8a62d24b060dc18a91
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-13 23:33:40 -0400

    avance

--- app/Services/Partido.php
+++ app/Services/Partido.php
@@ -5,6 +5,9 @@ use App\Services\Interfaces\Jugador;
 class Partido
 {
     public function competir(Jugador $jugador1,Jugador $jugador2){
+        foreach($jugador1->obtenerHabilidades() as $habilidad){
+            $roll = $habilidad->valor + rand(1,20);
+        }
         $ganador = $jugador1;
         return $ganador;
     }
