f6c69d6a08588ef80c0736071e27544955317940
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-13 20:57:23 -0400

    avance

--- app/Services/Torneo.php
+++ app/Services/Torneo.php
@@ -13,8 +13,8 @@ class Torneo
         shuffle($jugadores);
         $encuentros = array_chunk($jugadores,2);
         $ganadores  = [];
-        foreach($encuentros as $jugadores){
-            $ganadores[] = $partido->competir($jugadores[0],$jugadores[1]);
+        foreach($encuentros as $jugador){
+            $ganadores[] = $partido->competir($jugador[0],$jugador[1]);
         }
         return count($ganadores) != 2 ? 
             $this->eliminatorias($ganadores,$partido) : $ganadores;
