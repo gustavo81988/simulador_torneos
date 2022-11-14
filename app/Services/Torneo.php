4b5f2f3ec0d752f0696774f808b4bec18762bfa9
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-13 20:26:46 -0400

    refactoring

--- app/Services/Torneo.php
+++ app/Services/Torneo.php
@@ -9,8 +9,9 @@ class Torneo
 
     public function __construct(array $jugadores,Partido $partido){
         $this->jugadores = $jugadores;
-        $finalistas = $this->eliminatorias($jugadores);
-        $ganador    = $this->competir($finalistas[0],$finalistas[1]);
+        $this->partido   = $partido;
+        $finalistas      = $this->eliminatorias($jugadores);
+        $ganador         = $this->competir($finalistas[0],$finalistas[1]);
         return $ganador;
     }
 
@@ -19,16 +20,10 @@ class Torneo
         $encuentros = array_chunk($jugadores,2);
         $ganadores  = [];
         foreach($encuentros as $encuentro){
-            $jugador1 = $encuentro[0];
-            $jugador2 = $encuentro[1];
-            $ganadores[] = $this->competir($jugador1,$jugador2);
+            $jugador1    = $encuentro[0];
+            $jugador2    = $encuentro[1];
+            $ganadores[] = $this->partido->competir($jugador1,$jugador2);
         }
-
-        if(count($ganadores) != 2){
-            //se aplica recursividad hasta llegar a los finalistas
-            return $this->eliminatorias($ganadores);
-        }else{
-            return $ganadores;
-        }
+        return count($ganadores) != 2 ? $this->eliminatorias($ganadores): $ganadores;
     }
 }
