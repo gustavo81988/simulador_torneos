8c64663676b24a0cc3980917ada9b930327c0267
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-13 20:37:05 -0400

    refactoring

--- app/Services/Torneo.php
+++ app/Services/Torneo.php
@@ -5,24 +5,20 @@ use App\Services\Partido;
 class Torneo
 {
     protected $jugadores;
-    protected $partido;
 
     public function __construct(array $jugadores,Partido $partido){
         $this->jugadores = $jugadores;
-        $this->partido   = $partido;
-        $finalistas      = $this->eliminatorias($jugadores);
-        $ganador         = $this->competir($finalistas[0],$finalistas[1]);
+        $finalistas      = $this->eliminatorias($jugadores,$partido);
+        $ganador         = $partido->competir($finalistas[0],$finalistas[1]);
         return $ganador;
     }
 
-    protected function eliminatorias($jugadores){
+    protected function eliminatorias(array $jugadores,Partido $partido){
         shuffle($jugadores);
         $encuentros = array_chunk($jugadores,2);
         $ganadores  = [];
-        foreach($encuentros as $encuentro){
-            $jugador1    = $encuentro[0];
-            $jugador2    = $encuentro[1];
-            $ganadores[] = $this->partido->competir($jugador1,$jugador2);
+        foreach($encuentros as $jugadores){
+            $ganadores[] = $partido->competir($jugadores[0],$jugadores[1]);
         }
         return count($ganadores) != 2 ? $this->eliminatorias($ganadores): $ganadores;
     }
