d838313ed048209bd2c7ce8d188edc1cef16ce25
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-17 10:57:43 -0400

    eliminados campos innecesarios

--- app/Partido.php
+++ app/Partido.php
@@ -7,8 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Partido extends Model
 {
     protected $guarded = ['id'];
-
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
--- app/Torneo.php
+++ app/Torneo.php
@@ -9,8 +9,4 @@ class Torneo extends Model
     protected $fillable = [
         'nombre','created_at','updated_at'
     ];
-
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
