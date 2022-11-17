c295abdbafce3f7ba54c5c5fede40e9a8a737bc7
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-16 23:09:00 -0400

    agregado paraemtro torneo

--- app/Services/Torneo.php
+++ app/Services/Torneo.php
@@ -7,22 +7,22 @@ class Torneo
 {
     public function obtenerGanador(string $nombre,array $jugadores,Partido $partido){
         $torneo     = $this->crearTorneo($nombre);
-        $finalistas = $this->eliminatorias($jugadores,$partido);
-        return $partido->competir($finalistas);
+        $finalistas = $this->eliminatorias($jugadores,$partido,$torneo);
+        return $partido->competir($finalistas,$torneo);
     }
 
     public function crearTorneo(string $nombre){
         return ModelTorneo::create(['nombre'=> $nombre]);
     }
 
-    protected function eliminatorias(array $jugadores,Partido $partido):array{
-        // shuffle($jugadores);
+    protected function eliminatorias(array $jugadores,Partido $partido,$torneo):array{
+        shuffle($jugadores);
         $encuentros = array_chunk($jugadores,2);
         $ganadores  = [];
         foreach($encuentros as $participantes){
-            $ganadores[] = $partido->competir($participantes);
+            $ganadores[] = $partido->competir($participantes,$torneo);
         }
         return count($ganadores) != 2 ? 
-            $this->eliminatorias($ganadores,$partido) : $ganadores;
+            $this->eliminatorias($ganadores,$partido,$torneo) : $ganadores;
     }
 }
--- database/migrations/2022_11_17_025431_crear_tabla_partidos.php
+++ database/migrations/2022_11_17_025431_crear_tabla_partidos.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CrearTablaEncuentros extends Migration
+class CrearTablaPartidos extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CrearTablaEncuentros extends Migration
      */
     public function up()
     {
-        Schema::create('encuentros', function (Blueprint $table) {
+        Schema::create('partidos', function (Blueprint $table) {
             $table->id();
             $table->foreignId('id_torneo');
             $table->foreignId('id_jugador_1');
@@ -31,6 +31,6 @@ class CrearTablaEncuentros extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('encuentros');
+        Schema::dropIfExists('partidos');
     }
 }
