b7f5dc7302bc5162daf2cd95dc1e007a4828ee28
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-18 19:43:07 -0400

    refactoring

--- app/Usuario.php
+++ app/Usuario.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Services\JugadorMasculino;
+use App\Services\JugadorFemenino;
 use App\Services\Habilidad;
 
 class Usuario extends Authenticatable
@@ -40,6 +41,20 @@ class Usuario extends Authenticatable
                 new Habilidad('Habilidad',$jugador['habilidad'],3),
                 new Habilidad('Fuerza',$jugador['fuerza'],1),
                 new Habilidad('Velocidad',$jugador['velocidad'],3)
+            );
+        }
+        return $jugadores;
+    }
+
+    public function jugadoresFemeninos($usuarios){
+        $jugadores = [];
+        foreach($usuarios as $jugador){
+            $jugadores[] = new JugadorMasculino(
+                $jugador['id'],
+                $jugador['nombre'],
+                $jugador['apellido'],
+                new Habilidad('Habilidad',$jugador['habilidad'],1),
+                new Habilidad('Tiempo de Reaccion',$jugador['tiempo_reaccion'],1)
             );
         }
         return $jugadores;
