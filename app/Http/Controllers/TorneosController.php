099b52bba70e79af6918f61070b6578e81473a6c
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-17 22:58:50 -0400

    avance

--- app/Http/Controllers/TorneosController.php
+++ app/Http/Controllers/TorneosController.php
@@ -6,7 +6,16 @@ use Illuminate\Http\Request;
 
 class TorneosController extends Controller
 {
-    public function index(){
-        return view('torneos.index');
+    public function listarTorneos(Request $request)
+    {
+        $torneo = DB::table('torneos')
+            ->select(DB::raw("
+                *
+            "))
+            ->join('usuarios',function($join){
+                $join->on('usuarios.id','=','torneos.id_usuario_ganador');
+            })
+        ;
+        return response()->json($torneo);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::post('torneo_masculino','TorneoMasculinoController@index')->name('api.torneo_masculino.index');
 Route::post('torneo_femenino','TorneoFemeninoController@index')->name('api.torneo_femenino.index');
 
+Route::get('listar_torneos','TorneosController@index')->name('index');
+
 Route::post('usuario/guardar','UsuarioController@guardar')->name('api.usuario.guardar');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
