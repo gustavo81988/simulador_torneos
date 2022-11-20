03339576d3065d5085e7b1459dbafc4aaea32c41
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-20 14:08:09 -0400

    request torneo

--- app/Http/Controllers/TorneosController.php
+++ app/Http/Controllers/TorneosController.php
@@ -3,17 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\Torneo\ListarTorneosRequest;
+use App\Http\Requests\Torneo\DetalleTorneoRequest;
+use App\Http\Requests\Torneo\JugarTorneoRequest;
+
 use App\Usuario;
 use App\Torneo as ModelTorneo;
 use App\Services\TorneoEliminatorio;
 use App\Services\Partido;
-use App\Http\Requests\Torneo\ListarTorneosRequest;
-use App\Http\Requests\Torneo\DetalleTorneoRequest;
 
 class TorneosController extends Controller
 {
     public function jugar(
-        Request $request,Usuario $usuario,Partido $partido,TorneoEliminatorio $torneo
+        JugarTorneoRequest $request,Usuario $usuario,Partido $partido,TorneoEliminatorio $torneo
     ){
         $usuarios     = Usuario::whereIn('id',$request->jugadores)->get();
         $model_torneo = ModelTorneo::create(['nombre'=> $request->nombre_torneo,'genero'=> $request->genero]);
