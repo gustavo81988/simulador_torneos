3222c4a1a768a2d73bdb5017549a190afb83dae1
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-20 17:40:51 -0400

    Validaciones finales

--- app/Http/Requests/Torneo/JugarTorneoRequest.php
+++ app/Http/Requests/Torneo/JugarTorneoRequest.php
@@ -23,11 +23,12 @@ class JugarTorneoRequest extends FormRequest
     public function messages()
     {
         return [
-            'jugadores.min' => 'Debe ingresar al menos 4 :attribute.',
-            'genero.in'       => ':attribute debe ser M (Masculino) o F (Femenino).',
-            'genero.required' => 'El campo :attribute es obligatorio',
+            'jugadores.min'          => 'Debe ingresar al menos 4 :attribute.',
+            'jugadores.required'     => 'El campo :attribute es obligatorio',
+            'genero.in'              => ':attribute debe ser M (Masculino) o F (Femenino).',
+            'genero.required'        => 'El campo :attribute es obligatorio',
             'nombre_torneo.required' => 'El campo :attribute es obligatorio',
-            'nombre_torneo.unique' => 'Ya existe un :attribute con ese nombre.'
+            'nombre_torneo.unique'   => 'Ya existe un :attribute con ese nombre.'
         ];
     }
 
@@ -39,13 +40,14 @@ class JugarTorneoRequest extends FormRequest
     public function rules()
     {
         return [
-            // 'nombre_torneo' => 'unique:App\Torneo,nombre|required',
+            'nombre_torneo' => 'unique:App\Torneo,nombre|required',
             'jugadores' => [
                 new PotenciaDeDos($this->jugadores),
                 new ArrayValoresUnicos($this->jugadores),
                 new JugadoresInvalidos($this->jugadores),
                 new GeneroTorneo($this->jugadores,$this->genero),
-                'Min:4'
+                'Min:4',
+                'required'
             ],
             'genero' => 'in:M,F|required'
         ];
